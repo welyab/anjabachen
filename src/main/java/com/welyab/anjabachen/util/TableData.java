78f19647ad94c189ff6393538c93fc51b9d4a567
Welyab Paula <welyab.exe@gmail.com>
2019-07-24 09:30:48 -0300

    Javadoc the class.

--- src/main/java/com/welyab/anjabachen/util/TableData.java
+++ src/main/java/com/welyab/anjabachen/util/TableData.java
@@ -15,11 +15,36 @@
  */
 package com.welyab.anjabachen.util;
 
+/**
+ * This is a auxiliary class to serve as abstraction to the data source for the
+ * <code>TablePrinter</code>.
+ *
+ * @author Welyab Paula
+ */
 public interface TableData {
 
+	/**
+	 * Retrieve the total number of rows provided by this <code>TableData</code>.
+	 *
+	 * @return The total number of rows.
+	 */
 	public int rowCount();
-	
+
+	/**
+	 * Retrieves the total number of columns provider by this <code>TableData</code>.
+	 *
+	 * @return The total number of columns.
+	 */
 	public int columnCount();
 
+	/**
+	 * Retrieves the text data localized in the specific <code>[row, column]</code> position. These
+	 * location are zero based indexes.
+	 *
+	 * @param row The row number.
+	 * @param column The column number.
+	 *
+	 * @return The text value.
+	 */
 	public String getValue(int row, int column);
 }
